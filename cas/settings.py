dc9e7cb269a4f63aefd52bf8317a470b234d8063
Kevin Lee <leekevin@berkeley.edu>
2016-02-10 23:19:32 -0800

    Add django-debug-toolbar

--- cas/settings.py
+++ cas/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
