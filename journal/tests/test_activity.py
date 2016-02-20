b23b83c63341770a2da1a593fbd315821626474c
Kevin Lee <leekevin@berkeley.edu>
2016-02-19 17:23:22 -0800

    add activity serializer test

---
+++ journal/tests/test_activity.py
@@ -0,0 +1,24 @@
+import datetime
+
+from django.test import TestCase
+from journal.models import Activity, Entry
+from journal.serializers import ActivitySerializer
+
+
+class ActivityTestCase(TestCase):
+    """Sanity checks for activity"""
+    def setUp(self):
+        cat_e = Entry.objects.create(entry='I like walking the cat')
+        Activity.objects.create(name='Walking the cat',
+                                description='Walking the cat around the block',
+                                activity_type='CA', learning_obj='123',
+                                entries=cat_e,
+                                start_date=datetime.date.today)
+
+    def test_activity_serializer(self):
+        cat_activity = Activity.objects.get(name='Walking the cat')
+        self.assertEqual(cat_activity.name, 'Walking the cat')
+
+        cat_serializer = ActivitySerializer(cat_activity)
+        self.assertEqual(cat_serializer.data['description'],
+                         'Walking the cat around the block')
