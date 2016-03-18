4b09ab78be767fdf37f4cdf69a9824ad24021377
Shafqat Dulal <shafqat.dulal@gmail.com>
2016-03-17 18:13:29 -0700

    Make scripts smarter
    The db setup and remove scripts now include a check and branch
    depending on the operating system of the user.

--- scripts/db_remove.sh
+++ scripts/db_remove.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 set -e
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Please run me as root"
-    exit
-fi
+export OS=`uname`
 
-echo "Removing cas db"
-sudo su - postgres -c "dropdb cas"
-echo "Dropping cas_user"
-sudo su - postgres -c "dropuser cas_user"
+if [[  "${OS}" == "Darwin" ]]; then
+    echo "Removing cas db"
+    dropdb cas
+    echo "Dropping cas_user"
+    dropuser cas_user
+else
+    if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+        echo "Please run me as root"
+        exit
+    fi
+    echo "Removing cas db"
+    sudo su - postgres -c "dropdb cas"
+    echo "Dropping cas_user"
+    sudo su - postgres -c "dropuser cas_user"
+fi
--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 set -e
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Please run me as root"
-    exit
-fi
+export OS=`uname`
 
-echo "Creating cas db"
-sudo su - postgres -c "createdb cas"
-echo "Creating cas_user"
-sudo su - postgres -c "createuser --createdb -R -S cas_user"
-echo "Granting permissions"
-sudo su - postgres -c "psql -c 'GRANT ALL PRIVILEGES ON DATABASE cas TO cas_user;'"
+if [[ "${OS}" == "Darwin" ]]; then
+    echo "Creating cas db"z
+    createdb cas
+    echo "Creating cas_user"
+    createuser --createdb -R -S cas_user
+else
+    if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+        echo "Please run me as root"
+        exit
+    fi
+    echo "Creating cas db"
+    sudo su - postgres -c "createdb cas"
+    echo "Creating cas_user"
+    sudo su - postgres -c "createuser --createdb -R -S cas_user"
+fi
