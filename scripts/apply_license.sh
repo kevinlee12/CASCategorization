fffd5c91d33b9644f3b2045d26db3c221992a46c
Shafqat Dulal <shafqat.dulal@gmail.com>
2016-04-08 23:37:07 -0700

    Exclude manage.py from header applier

--- scripts/apply_license.sh
+++ scripts/apply_license.sh
@@ -4,7 +4,7 @@ starting_dir=$(pwd)
 license_num_lines=$(wc -l < scripts/license_header)
 license_header=$(cat scripts/license_header)
 
-for f in $(find . -name '*.py' | grep -vE '__|migrations');
+for f in $(find . -name '*.py' | grep -vE '__|migrations|manage\.py');
 do
 	top_of_file=$(head -n ${license_num_lines} $f)
 	if [[ "${top_of_file}" != "${license_header}" ]]; then
